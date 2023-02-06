9e7a9523c007c4d37c60a90875b98762573335d6
Daniel Dinnyes <dadinn@besenczy.net>
2023-02-06 02:58:12 +0000

    extracted Live ISO specs into separate resource file

---
+++ resources/isos.scm
@@ -0,0 +1,20 @@
+(("debian" .
+  (("stretch" .
+    (("http"
+      (("filename" . "debian-live-9.2.0-amd64-gnome.iso")))))
+   ("buster" .
+    (("torrent" .
+      (("filename" . "debian-live-10.3.0-amd64-standard.iso")
+       ("url" . "magnet:?xt=urn:btih:7bf9f33a7cc577b7829a4b9db8fe89dacd6eabd9&dn=debian-live-10.10.0-amd64-standard.iso&tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce")))))
+   ("bullseye" .
+    (("http" .
+      (("filename" . "debian-live-11.6.0-amd64-standard.iso")
+       ("url" . "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-11.6.0-amd64-standard.iso")))
+     ("torrent" .
+      (("filename" . "debian-live-11.6.0-amd64-standard.iso")
+       ("url" . "magnet:?xt=urn:btih:4365ce1cbb930a7c018e70073fdb2877bd4da852&dn=debian-live-11.6.0-amd64-standard.iso&tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce")))))))
+ ("archlinux" .
+  (("2020.01.01" .
+    (("http" .
+      (("filename" ."archlinux-2020.01.01-x86_64.iso")
+       ("url" ."https://archive.archlinux.org/iso/2020.01.01/archlinux-2020.01.01-x86_64.iso"))))))))
